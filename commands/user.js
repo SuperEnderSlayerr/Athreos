7cffa5240588d5a7b4f5265e643631d3c46967b0
Clara <claranoelenightingale@gmail.com>
2022-12-23 17:39:31 -0700

    update

--- commands/user.js
+++ commands/user.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { defembed } = require('../functions/defaultembed');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,8 +8,10 @@ module.exports = {
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		const embed = new EmbedBuilder()
-			.setDescription(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
-		await interaction.reply({embeds: [embed]});
+		const embed = defembed(
+			`Message Interaction!`,
+			`User <@!${message.author.id}> sent a message in channel <#${historyData[`${message.author.id}`]}>`,
+			message.author.displayAvatarURL())
+		channel.send({embeds: [embed]})
 	},
 };
