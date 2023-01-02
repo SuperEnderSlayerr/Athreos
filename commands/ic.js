87e08aec6af466c64e77831955997d12a73d00ff
SuperEnderSlayerr <84186594+SuperEnderSlayerr@users.noreply.github.com>
2023-01-01 20:39:34 -0700

    Added exporting for the historyData variable.

--- commands/ic.js
+++ commands/ic.js
@@ -8,7 +8,7 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 		const sucBool = remove(interaction.channelId);
-		if (sucBool) await interaction.editReply("Successfully removed this channel from the list of out of character channels!");
+		if (sucBool) await interaction.editReply("Successfully removed this channel from the list of out of character channels!\n\nRemember to reset the bot before this will take effect.");
 		else await interaction.editReply("This channel was not in the list of out of character channels.");
 	},
 };
--- commands/ooc.js
+++ commands/ooc.js
@@ -9,6 +9,6 @@ module.exports = {
 		await interaction.deferReply();
 		const sucBool = add(interaction.channelId);
 		if (sucBool) await interaction.editReply("This channel was already in the list of out of character channels.");
-		else await interaction.editReply("Successfully added this channel to the list of out of character channels!");
+		else await interaction.editReply("Successfully added this channel to the list of out of character channels!\n\nRemember to reset the bot before this will take effect.");
 	},
 };
