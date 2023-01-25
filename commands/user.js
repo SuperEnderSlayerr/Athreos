ed0da6b494c4142b805d1cdb56c36518eb89437d
ClaraNoele <claranoelenightingale@gmail.com>
2023-01-25 14:45:59 -0700

    changes to defaultembed, added file for library command structure

--- commands/user.js
+++ commands/user.js
@@ -8,11 +8,13 @@ module.exports = {
 	async execute(interaction) {
 		// interaction.user is the object representing the User who ran the command
 		// interaction.member is the GuildMember object, which represents the user in the specific guild
-		const embed = defembed(
-			`Message Interaction!`,
-			`User <@!${interaction.user.id}> sent a message in channel <#${interaction.channelId}>`,
-			interaction.user.displayAvatarURL(),
-			interaction)
+		const embed = defembed({
+			title: `Message Interaction!`,
+			description: `User <@!${interaction.user.id}> sent a message in channel <#${interaction.channelId}>`,
+			thumbnail: interaction.user.displayAvatarURL(),
+			author: {name: 'Athreos', iconURL: interaction.client.user.displayAvatarURL()},
+			fields: ({name: "Creators!",value: "Ender and Clara"},{name: "Testing",value: "Yeet",inline: true},{name: "Yeet",value: "Value!",inline: true}),
+			interaction: interaction})
 		await interaction.reply({embeds: [embed]})
 	},
 };
