72d01d8b29b322723fee5f8fa37091ac8eab7756
SuperEnderSlayerr <84186594+SuperEnderSlayerr@users.noreply.github.com>
2023-02-21 22:14:38 -0700

    It works! Again ig.

--- websocket/handler.js
+++ websocket/handler.js
@@ -3,5 +3,6 @@ const messageHandler = require("../data/random/messageHandler");
 module.exports = (data) => {
 	const strData = String(data);
 	const useData = JSON.parse(strData);
+	if (useData.discordId === '') console.log("Discord ID is empty.");
 	messageHandler(useData);
 };
